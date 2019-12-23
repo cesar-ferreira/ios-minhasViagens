ff2212cd90f119020b562da422a1095dc3bc07b2
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2019-12-23 13:54:43 -0300

    setup autorization of location

--- Minhas Viagens/Minhas Viagens/ViewController.swift
+++ Minhas Viagens/Minhas Viagens/ViewController.swift
@@ -7,13 +7,52 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupLocationManager()
     }
+    
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if ( status != .authorizedWhenInUse ) {
+            
+            let alertController = UIAlertController(title: "Permissão de localização", message: "Necessário permissão para acesso a sua localização", preferredStyle: .alert)
+            
+            let actionConfig = UIAlertAction(title: "Abrir configurações", style: .default) { (UIAlertAction) in
+                
+                if let settingsUrl = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(settingsUrl)
+                }
+                
+            }
+            let actionCancel = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+            
+            alertController.addAction(actionConfig)
+            alertController.addAction(actionCancel)
+            
+            present( alertController, animated: true, completion: nil )
+            
+        }
+        
+    }
+    
 
 
 }
